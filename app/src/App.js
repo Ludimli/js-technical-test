f831319ce22dd51e9aade7c596e41600b9d97c88
Ludimli <ludovic.tricot@gmail.com>
2017-03-16 14:08:08 +0100

    Environment setup
    Setup environment with create-react-app + some cleaning

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-info">
+          Here will be the filters and the pie chart!
+        </div>
+        <div className="App-thread">
+          Here must be displayed the thread messages...
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
